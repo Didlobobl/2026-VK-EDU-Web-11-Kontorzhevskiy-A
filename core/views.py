5c3df9f5bd91a903444bad8815bf35d47f83e56e
Didlobobl <chelyaka@mail.ru>
2026-04-08 01:33:36 +0300

    Настроил vievs

--- core/views.py
+++ core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def login(request):
+    return render(request, 'core/login.html')
+
+def signup(request):
+    return render(request, 'core/signup.html')
+
+def profile(request):
+    user_data = {
+        'username': 'dr_house',
+        'email': 'house@pup.com',
+        'nickname': 'Gregory'
+    }
+    return render(request, 'core/profile.html', {'user': user_data})
--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
-# Create your views here.
+QUESTIONS = [
+    {
+        'id': i,
+        'title': f'Заголовок вопроса №{i}',
+        'content': f'Текст вопроса №{i}. Здесь должно быть подробное описание проблемы...',
+        'tags': ['python', 'django', 'web'],
+        'answers_count': i % 5,
+        'likes': i * 2,
+    } for i in range(1, 50)  
+]
+
+ANSWERS = [
+    {
+        'id': i,
+        'content': f'Текст ответа №{i}. Очень полезный и правильный совет!',
+        'is_correct': i == 1, 
+    } for i in range(1, 10)
+]
+
+
+def index(request):
+    return render(request, 'questions/index.html', {'questions': QUESTIONS})
+
+def hot(request):
+    hot_questions = sorted(QUESTIONS, key=lambda x: x['likes'], reverse=True)
+    return render(request, 'questions/index.html', {'questions': hot_questions})
+
+def tag(request, tag_name):
+    tag_questions = [q for q in QUESTIONS if tag_name in q['tags']]
+    return render(request, 'questions/tag.html', {
+        'tag_name': tag_name,
+        'questions': tag_questions
+    })
+
+def question(request, question_id):
+    item = QUESTIONS[question_id - 1] 
+    return render(request, 'questions/question.html', {
+        'question': item,
+        'answers': ANSWERS
+    })
+
+def ask(request):
+    return render(request, 'questions/ask.html')
