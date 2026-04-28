f3a9d83c566516bd61d0a28a27c367af2fddf5fd
Didlobobl <chelyaka@mail.ru>
2026-04-28 18:46:24 +0300

    Настроил форму входа и регистрации

--- application/settings.py
+++ application/settings.py
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
 LANGUAGE_CODE = 'ru-ru'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC +3'
 USE_I18N = True
 USE_TZ = True
 
