95661a350389ea7cf5de928e309cf4d74b717692
Didlobobl <chelyaka@mail.ru>
2026-04-28 17:53:41 +0300

    Настроил форму регистрации

---
+++ core/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from .models import Profile
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label="Логин", widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label="Пароль", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+class SignupForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password_confirm = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+
+        if password and password_confirm and password != password_confirm:
+            self.add_error('password_confirm', "Пароли не совпадают")
+        
+        # Запускаем стандартные валидаторы Django
+        if password:
+            try:
+                validate_password(password)
+            except ValidationError as e:
+                self.add_error('password', e)
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+            Profile.objects.create(user=user) # Автоматически создаем профиль!
+        return user
--- core/views.py
+++ core/views.py
@@ -1,16 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.conf import settings
+from .forms import LoginForm
 
 def login(request):
-    return render(request, 'core/login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(request, username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            if user is not None:
+                auth_login(request, user)
+                next_url = request.GET.get('next') or request.POST.get('next') or 'questions:index'
+                if url_has_allowed_host_and_scheme(next_url, allowed_hosts={request.get_host()}):
+                    return redirect(next_url)
+                return redirect('questions:index')
+            else:
+                form.add_error(None, "Неверный логин или пароль") 
+    else:
+        form = LoginForm()
+
+    return render(request, 'core/login.html', {'form': form})
+
+def logout(request):
+    auth_logout(request)
+    return redirect(request.META.get('HTTP_REFERER', 'questions:index'))
 
 def signup(request):
-    return render(request, 'core/signup.html')
-
-def profile(request):
-    user_data = {
-        'username': 'dr_house',
-        'email': 'house@pup.com',
-        'nickname': 'Gregory'
-    }
-    return render(request, 'core/profile.html', {'user': user_data})
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user) 
+            return redirect('questions:index')
+    else:
+        form = SignupForm()
+    return render(request, 'core/signup.html', {'form': form})
