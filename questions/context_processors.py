bd1c22a52df7187bb849b4686ea443fc3c444d22
Didlobobl <chelyaka@mail.ru>
2026-05-06 15:21:38 +0300

    Изменил README

--- questions/context_processors.py
+++ questions/context_processors.py
@@ -8,8 +8,6 @@ def sidebar_data(request):
         questions_count=Count('questions')
     ).order_by('-questions_count')[:20]
 
-    # Топ 10 пользователей (упрощенно: по количеству созданных ими вопросов)
-    # В будущем здесь можно считать по рейтингу лайков за неделю, как требует ТЗ
     best_members = User.objects.annotate(
         q_count=Count('question')
     ).order_by('-q_count')[:10]
